0aadd798842df1c21ad6c2e0a2ace451ce99b8a8
planck <987379867@qq.com>
2024-05-28 21:28:15 +0800

    init

---
+++ src/main/java/com/easypan/ChatRobotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.easypan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRobotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRobotBackendApplication.class, args);
+    }
+
+}
